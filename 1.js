1d35d55c494d288b4dab6bdd57b4e888bc8234a8
liushengzu <liushengzu88@gmail.com>
2025-07-19 14:25:35 +0800

    修改

--- 1.js
+++ 1.js
@@ -96,14 +96,28 @@ function demo_pro() {
     let brr1 = arr.reduce(function (acc, cur, index, arr) {
         // 执行语句
         //	console.log(acc,cur,index,arr);
-        console.log(acc);
-        console.log(cur);
+        // console.log(acc);
+        // console.log(cur);
         return acc + cur;
     }, 0);
+    console.log(brr1);
 }
 // 外部调用 demo_pro()函数，输出结果
-console.log(demo_pro());
+// console.log(demo_pro());
+// // console.log(brr1);
+// console.
+// array.forEach(element => {
 
-
-
-
+// });
+const obj = { a: 1, b: 2, c: 3 };
+Object.keys(obj).forEach((key, index) => {
+    // key.value=value;
+    // 取key对应对象的值 
+    // console.log(key,index);
+    console.log(key, obj[key], index);
+});
+Object.entries([1, 2, 3], [4, 5, 6]).flatMap(value => {
+    console.log(value);
+});
+a=b??c; 
+a=b||c; 
