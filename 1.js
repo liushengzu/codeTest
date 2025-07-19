492c01008202616cc440d51967b287d58ca70752
liushengzu <liushengzu88@gmail.com>
2025-07-19 14:53:31 +0800

    修改

--- 1.js
+++ 1.js
@@ -1,5 +1,7 @@
 // const { values } = require("lodash");
 
+const { forEach } = require("lodash");
+
 // console.log("Hello, world!");
 // let arr = [1, 2, 3, 4, 5];
 // // console.log(arr);
@@ -40,7 +42,6 @@
 // 7.some()方法测试数组中是否至少有一个元素通过所提供的测试函数。
 // require('dayjs/locale/zh-cn');
 
-
 // ****************************************************
 // import _ from 'lodash'; // 导入lodash
 // import dayjs from 'dayjs';// 导入dayjs
@@ -62,12 +63,12 @@
 //         // 打印输出元素
 //         console.log(`第${row_Index+1}行，第${col_Index+1}列:`,cell_Val)
 //                 }
-//                 )})    
+//                 )})
 //     }
 
 // ******************************************
 // let arr = [[12, 12, 13, 14, 15, 16], [12, 12, 13, 14, 15, 16], [12, 12, 13, 14, 15, 16]];
-// let arr2=arr.map((row,row_Index)=>{ 
+// let arr2=arr.map((row,row_Index)=>{
 //     return row.map((cell_Val,col_Index)=>{
 //         return cell_Val*2;
 //     })
@@ -77,30 +78,34 @@
 // console.log(arr2);
 // reduce()求和
 function demo_pro() {
-    let arr = [[1, 2, 3], [4, 5, 5], [7, 8, 9]];
-    // let brr = arr.reduce((acc, cur, index, arr) => { // acc累加器 cur当前值 index当前索引 arr数组
-    //     // 执行语句
-    //     //	console.log(acc,cur,index,arr);
-    //     console.log(acc);
-    //     console.log(cur);
-    //     //		console.log(arr[index]);0,1,2运行完成
-    //     //		console.log(arr.value2);
-    //     // 数组3个，分三次运行
-    //     // 第一次 acc=0,cur=[1,2,3]；第二次运行 acc="01,2,3"，cur=[4,5,5]，第三次acc="01,2,34,5,5"，cur=[7,8,9]
-    //     // 第四次运行 acc="01,2,34,5,57,8,9"
-    //     return acc + cur;// 第一次运行完：acc+cur= "01,2,3"，第二次运行完：acc+cur="01,2,34,5,5"，第三次运行完：acc+cur="01,2,34,5,57,8,9"
-    //     // 把结果返回给brr数组
-    // }, "");
-    // reduce() 对数组进行累加，返回累加后的结果 
+  let arr = [
+    [1, 2, 3],
+    [4, 5, 5],
+    [7, 8, 9],
+  ];
+  // let brr = arr.reduce((acc, cur, index, arr) => { // acc累加器 cur当前值 index当前索引 arr数组
+  //     // 执行语句
+  //     //	console.log(acc,cur,index,arr);
+  //     console.log(acc);
+  //     console.log(cur);
+  //     //		console.log(arr[index]);0,1,2运行完成
+  //     //		console.log(arr.value2);
+  //     // 数组3个，分三次运行
+  //     // 第一次 acc=0,cur=[1,2,3]；第二次运行 acc="01,2,3"，cur=[4,5,5]，第三次acc="01,2,34,5,5"，cur=[7,8,9]
+  //     // 第四次运行 acc="01,2,34,5,57,8,9"
+  //     return acc + cur;// 第一次运行完：acc+cur= "01,2,3"，第二次运行完：acc+cur="01,2,34,5,5"，第三次运行完：acc+cur="01,2,34,5,57,8,9"
+  //     // 把结果返回给brr数组
+  // }, "");
+  // reduce() 对数组进行累加，返回累加后的结果
 
-    let brr1 = arr.reduce(function (acc, cur, index, arr) {
-        // 执行语句
-        //	console.log(acc,cur,index,arr);
-        // console.log(acc);
-        // console.log(cur);
-        return acc + cur;
-    }, 0);
-    console.log(brr1);
+  let brr1 = arr.reduce(function (acc, cur, index, arr) {
+    // 执行语句
+    //	console.log(acc,cur,index,arr);
+    // console.log(acc);
+    // console.log(cur);
+    return acc + cur;
+  }, 0);
+  console.log(brr1);
 }
 // 外部调用 demo_pro()函数，输出结果
 // console.log(demo_pro());
@@ -111,13 +116,30 @@ function demo_pro() {
 // });
 const obj = { a: 1, b: 2, c: 3 };
 Object.keys(obj).forEach((key, index) => {
-    // key.value=value;
-    // 取key对应对象的值 
-    // console.log(key,index);
-    console.log(key, obj[key], index);
+  // key.value=value;
+  // 取key对应对象的值
+  // console.log(key,index);
+  console.log(key, obj[key], index);
 });
-Object.entries([1, 2, 3], [4, 5, 6]).flatMap(value => {
-    console.log(value);
+Object.entries([1, 2, 3], [4, 5, 6]).flatMap((value) => {
+  console.log(value);
 });
-a=b??c; 
-a=b||c; 
+
+forEach([1, 2, 3], (value, index) => {
+  console.log(value, index);
+});
+
+function test() {
+let arr = [1, 2, 3, 4, 5];
+let newArr = arr.map((value, index) => {
+value = value * 2;
+return value;
+});
+let sum = newArr.reduce((acc, cur) => {
+// acc累加器 cur当前值
+return acc + cur;
+}, 0);
+console.log(sum);
+console.log(newArr);
+}
+test();
